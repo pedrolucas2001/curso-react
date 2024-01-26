796c98ac015cb3a7daa4e20258e67c591f1f375f
LJITDPDF <ljitdpdf@gmail.com>
2024-01-26 20:42:29 -0300

    Criando componentes dinâmicos com props e children

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
+import Banner from "./components/Banner";
+import Container from "./components/Container";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 
 function App() {
   return (
     <>
-      <Header/>
-      <h1>Hello World</h1>
-      <p>bla bla bla bla</p>
-      <Footer/>
+      <Header />
+      <Banner image="nada" />
+      <Container>
+        <h1>Hello World</h1>
+        <p>bla bla bla bla</p>
+      </Container>
+      <Footer />
     </>
     
   );
