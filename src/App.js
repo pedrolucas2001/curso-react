bdf05f78cabceec041760c3490b2a5344ebb6497
LJITDPDF <ljitdpdf@gmail.com>
2024-01-26 10:36:06 -0300

    Iniciando estudos em ReactJS

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <h1>Hello World</h1>
+      <p>bla bla bla bla</p>
+      <Footer/>
+    </>
+    
+  );
+}
+
+export default App;
---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import style from "./Footer.module.css"
+
+function Footer(){
+    return(
+        <footer className={style.footer}>
+            <h2>
+                PobreFlix &copy; Desenvolvido por Pedro L 2023
+            </h2>
+        </footer>
+    );
+};
+
+export default Footer;
